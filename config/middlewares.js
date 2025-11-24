f3244b7557b9d41b02ad2b4e85d2e4f6662b9f95
digimagweb <digimag.co.in@gmail.com>
2025-11-24 23:18:03 +0530

    middleware.js changed for CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,23 +1,26 @@
 // config/middlewares.js
 module.exports = [
   'strapi::errors',
-  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
-      // Remove this: enabled: true,
-      headers: '*',
+      enabled: true,
       origin: [
         'http://localhost:3000',
-        'http://localhost:1337', 
+        'http://localhost:1337',
         'http://localhost:5173',
+        'http://admin.metamag.co.in',
+        'https://admin.metamag.co.in',
         'https://metamag.co.in',
         'https://www.metamag.co.in',
-        'https://admin.metamag.co.in',
         'https://appetizing-reward-d32d5c8f0f.strapiapp.com'
-      ]
+      ],
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
+      credentials: true
     }
   },
+  'strapi::security',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
